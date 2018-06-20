490df769d094eee4cda51fda8ae2cb0acc78a83b
Felix Pojtinger <felix@pojtinger.com>
2018-06-20 17:59:30 +0200

    Added boilderplate, tested some components

---
+++ blueprint-eval/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
