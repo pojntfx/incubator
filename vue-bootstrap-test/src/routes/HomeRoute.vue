f7fa4e797b60bd7076731012e3f15f706566eb0b
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-06-27 19:13:59 +0200

    Added simple example navbar

---
+++ vue-bootstrap-test/src/routes/HomeRoute.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  HomePage
+</template>
+
+<script>
+import HomePage from "@/pages/HomePage";
+
+export default {
+  name: "HomeRoute",
+  components: {
+    HomePage
+  }
+};
+</script>
