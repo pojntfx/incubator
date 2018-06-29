ee122be84063e388802665c941e5d468e1a12bb7
Felix Pojtinger <felix@pojtinger.com>
2018-06-29 20:29:25 +0200

    Added navigation structure

---
+++ vuetify-mockup/src/components/MainNavigation.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  v-navigation-drawer(
+      persistent
+      :mini-variant="isMiniVariant" 
+      :value="drawerIsOpen"
+      fixed
+      app
+      enable-resize-watcher
+      v-resize="handleResize")
+      v-list
+        v-list-tile(
+          value="true" 
+          v-for="(item, index) in navigationItems"
+          :key="index"
+          :to="item.href")
+          v-list-tile-action
+            v-icon(v-html="item.icon")
+          v-list-tile-content
+            v-list-tile(v-text="item.title")
+</template>
+
+<script>
+export default {
+  name: "MainNavigation",
+  props: {
+    miniVariant: Boolean,
+    items: Array,
+    drawer: Boolean
+  },
+  methods: {
+    handleResize() {
+      this.$emit("resize");
+    }
+  },
+  computed: {
+    isMiniVariant() {
+      return this.miniVariant;
+    },
+    navigationItems() {
+      return this.items;
+    },
+    drawerIsOpen() {
+      return this.drawer;
+    }
+  }
+};
+</script>
