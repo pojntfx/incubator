d44e3da7519c99302d5c0dc0d944c06c2a34de56
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-09-07 01:22:16 +0200

    feat(material-components-web-react-dashboard): add boilerplate

---
+++ packages/material-components-web-react-dashboard/src/components/Button.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { Button as ButtonTemplate } from "rmwc/Button";
+import "@material/button/dist/mdc.button.css";
+
+const Button = () => <ButtonTemplate>Test</ButtonTemplate>;
+
+export { Button };
