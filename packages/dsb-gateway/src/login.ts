a509755e4318240938dbe092fe29c553d445693f
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-10-03 20:22:40 +0200

    feat: login to dbsmobile

--- packages/dsb-gateway/src/login.ts
+++ packages/dsb-gateway/src/login.ts
@@ -1,5 +1,19 @@
+const USERNAME_INPUT_SELECTOR = "#txtUser";
+const PASSWORD_INPUT_SELECTOR = "#txtPass";
+const CREDENTIALS_SUBMIT_BUTTON = "#divLoginBox > input";
+
 const login = async (page, endpoint, username, password) => {
   await page.goto(endpoint);
+
+  await page.click(USERNAME_INPUT_SELECTOR);
+  await page.keyboard.type(username);
+
+  await page.click(PASSWORD_INPUT_SELECTOR);
+  await page.keyboard.type(password);
+
+  await page.click(CREDENTIALS_SUBMIT_BUTTON);
+
+  await page.waitForNavigation();
   await page.screenshot({ path: "dsbmobile.png" });
 };
 
