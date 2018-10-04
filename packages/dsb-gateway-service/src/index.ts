c42547176fa40cab42b8dcd27710db3dc4fb700a
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-10-04 15:29:48 +0200

    feat(dsb-gateway-service): add info endpoint

--- packages/dsb-gateway-service/src/index.ts
+++ packages/dsb-gateway-service/src/index.ts
@@ -1,4 +1,19 @@
+import { send } from "micro";
+import { router, get } from "micro-fork";
 import { fetchDataFromDSB } from "./handlers/fetchDataFromDSB";
+import * as query from "micro-query";
 
-export default async () =>
-  fetchDataFromDSB().then(() => "New Data has been fetched.");
+const state = {
+  lastFetchFromDSBDate: new Date(0)
+};
+
+const fetchUpdates = async (req, res) =>
+  query(req).nocache
+    ? await fetchDataFromDSB()
+        .then(() => (state.lastFetchFromDSBDate = new Date()))
+        .then(() =>
+          send(res, 200, { lastFetchFromDSBDate: state.lastFetchFromDSBDate })
+        )
+    : send(res, 200, { lastFetchFromDSBDate: state.lastFetchFromDSBDate });
+
+export default router()(get("/info", fetchUpdates));
