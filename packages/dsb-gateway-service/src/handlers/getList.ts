aa0d3f7bd661391402dd7b193af5596460610847
Felix Pojtinger <felix@pojtinger.com>
2018-10-04 17:48:26 +0200

    feat(dsb-gateway-service): add nocache param to list

--- packages/dsb-gateway-service/src/handlers/getList.ts
+++ packages/dsb-gateway-service/src/handlers/getList.ts
@@ -1,12 +1,38 @@
 import { getImageNames } from "../actions/getImageNames";
 
 const getList = async (
-  _,
+  req,
   res,
-  { imagesPath, endpoint, send, dirReader, fileStatisticsGetter }
+  {
+    db,
+    dataFetcher,
+    imagesPath,
+    endpoint,
+    send,
+    dirReader,
+    fileStatisticsGetter
+  }
 ) =>
-  await getImageNames(imagesPath, endpoint, dirReader, fileStatisticsGetter)
-    .then(fileNamesWithStatistics => send(res, 200, fileNamesWithStatistics))
-    .catch(e => send(res, 400, `Could not get list of images: ${e}`));
+  req.query.nocache
+    ? await dataFetcher()
+        .then(() => db.update("lastFetchFromDSBDate", new Date()).write())
+        .then(
+          async () =>
+            await getImageNames(
+              imagesPath,
+              endpoint,
+              dirReader,
+              fileStatisticsGetter
+            )
+        )
+        .then(fileNamesWithStatistics =>
+          send(res, 200, fileNamesWithStatistics)
+        )
+        .catch(e => send(res, 400, `Could not get list of images: ${e}`))
+    : await getImageNames(imagesPath, endpoint, dirReader, fileStatisticsGetter)
+        .then(fileNamesWithStatistics =>
+          send(res, 200, fileNamesWithStatistics)
+        )
+        .catch(e => send(res, 400, `Could not get list of images: ${e}`));
 
 export { getList };
