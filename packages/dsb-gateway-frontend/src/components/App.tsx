473a41b69bfde7bd2fc4046eb46322647b26afca
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-10-04 18:41:18 +0200

    feat(dsb-gateway-frontend): add image list

--- packages/dsb-gateway-frontend/src/components/App.tsx
+++ packages/dsb-gateway-frontend/src/components/App.tsx
@@ -1,27 +1,20 @@
 import * as React from "react";
 import { Container, Card, CardColumns } from "react-bootstrap";
 import { Navbar } from "./Navbar";
+import { ImageList } from "./ImageList";
 
-const ImgCard = ({ url, lastUpdate }) => (
-  <a href={url}>
-    <Card className="bg-primary text-white">
-      <Card.Img src={url} alt="Card image" />
-      <Card.ImgOverlay>
-        <Card.Text>Last updated {lastUpdate}</Card.Text>
-      </Card.ImgOverlay>
-    </Card>
-  </a>
-);
+const images = [
+  {
+    src:
+      "https://images.unsplash.com/photo-1538593315427-b14435fc9d3c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3058532fd7e4f7ef35b4c159890cf82e&auto=format&fit=crop&w=400&h=400",
+    lastUpdate: "3 mins ago"
+  }
+];
 
 const App = () => (
   <Container>
     <Navbar />
-    <CardColumns>
-      <ImgCard
-        url="https://images.unsplash.com/photo-1538593315427-b14435fc9d3c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3058532fd7e4f7ef35b4c159890cf82e&auto=format&fit=crop&w=400&h=400"
-        lastUpdate="3 mins ago"
-      />
-    </CardColumns>
+    <ImageList images={images} />
   </Container>
 );
 
