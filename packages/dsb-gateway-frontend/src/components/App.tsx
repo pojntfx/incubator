1ddb4c820d2b914a040bd46b68daeb56997927bd
Felix Pojtinger <felix@pojtinger.com>
2018-10-04 18:36:18 +0200

    feat(dsb-gateway-frontend): add simple image card

--- packages/dsb-gateway-frontend/src/components/App.tsx
+++ packages/dsb-gateway-frontend/src/components/App.tsx
@@ -1,5 +1,28 @@
 import * as React from "react";
+import { Container, Card, CardColumns } from "react-bootstrap";
+import { Navbar } from "./Navbar";
 
-const App = () => <h1>Hello, world!</h1>;
+const ImgCard = ({ url, lastUpdate }) => (
+  <a href={url}>
+    <Card className="bg-primary text-white">
+      <Card.Img src={url} alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Text>Last updated {lastUpdate}</Card.Text>
+      </Card.ImgOverlay>
+    </Card>
+  </a>
+);
+
+const App = () => (
+  <Container>
+    <Navbar />
+    <CardColumns>
+      <ImgCard
+        url="https://images.unsplash.com/photo-1538593315427-b14435fc9d3c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3058532fd7e4f7ef35b4c159890cf82e&auto=format&fit=crop&w=400&h=400"
+        lastUpdate="3 mins ago"
+      />
+    </CardColumns>
+  </Container>
+);
 
 export { App };
---
+++ packages/dsb-gateway-frontend/src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { Button } from "react-bootstrap";
+import styled from "styled-components";
+
+const NavbarWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Navbar = () => (
+  <NavbarWrapper className="my-3">
+    <Button variant="outline-primary" className="mr-auto">
+      Reload
+    </Button>
+    <Button variant="outline-primary" className="ml-auto">
+      Logout
+    </Button>
+  </NavbarWrapper>
+);
+
+export { Navbar };
