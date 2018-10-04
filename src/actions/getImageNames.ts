24fb914d149502c0293e0b183483df9bea8ecd22
Felix Pojtinger <felix@pojtinger.com>
2018-10-04 17:32:23 +0200

    feat(dsb-gateway-service): add img endpoint to list endpoint

--- src/actions/getImageNames.ts
+++ src/actions/getImageNames.ts
@@ -1,8 +1,14 @@
-const getImageNames = async (imagesPath, dirReader, fileStatisticsGetter) =>
+const getImageNames = async (
+  imagesPath,
+  endpoint,
+  dirReader,
+  fileStatisticsGetter
+) =>
   await dirReader(imagesPath).then(fileNames =>
     Promise.all(
       fileNames.map(async imageName => ({
-        path: imageName,
+        url: `${endpoint}/img/${imageName}`,
+        fileName: imageName,
         lastUpdate: await fileStatisticsGetter(imagesPath + imageName).then(
           stats => stats.mtime
         )
--- src/handlers/getList.ts
+++ src/handlers/getList.ts
@@ -3,9 +3,9 @@ import { getImageNames } from "../actions/getImageNames";
 const getList = async (
   _,
   res,
-  { imagesPath, send, dirReader, fileStatisticsGetter }
+  { imagesPath, endpoint, send, dirReader, fileStatisticsGetter }
 ) =>
-  await getImageNames(imagesPath, dirReader, fileStatisticsGetter)
+  await getImageNames(imagesPath, endpoint, dirReader, fileStatisticsGetter)
     .then(fileNamesWithStatistics => send(res, 200, fileNamesWithStatistics))
     .catch(e => send(res, 400, `Could not get list of images: ${e}`));
 
