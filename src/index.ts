c8ab13ea9cff375534696506c8fdec7eb26833de
Felix Pojtinger <felix@pojtinger.com>
2018-10-04 16:00:41 +0200

    feat(dsb-gateway-server): use lowdb as database

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 import { send } from "micro";
 import { router, get } from "micro-fork";
-import { fetchDataFromDSB } from "./handlers/fetchDataFromDSB";
+import { fetchImagesFromDSB } from "./actions/fetchImagesFromDSB";
 import * as query from "micro-query";
+import { startDatabase } from "./utils/db";
+import { updateList } from "./handlers/updateList";
+import * as low from "lowdb";
+import * as FileSync from "lowdb/adapters/FileSync";
+import { imagesPath, dbFilePath } from "./config.json";
 
-const state = {
-  lastFetchFromDSBDate: new Date(0)
-};
+const db = startDatabase(dbFilePath, low, FileSync);
+const dataFetcher = () => fetchImagesFromDSB(imagesPath);
 
-const fetchUpdates = async (req, res) =>
-  query(req).nocache
-    ? await fetchDataFromDSB()
-        .then(() => (state.lastFetchFromDSBDate = new Date()))
-        .then(() =>
-          send(res, 200, { lastFetchFromDSBDate: state.lastFetchFromDSBDate })
-        )
-    : send(res, 200, { lastFetchFromDSBDate: state.lastFetchFromDSBDate });
-
-export default router()(get("/info", fetchUpdates));
+export default router()(
+  get("/info", updateList, {
+    db,
+    dataFetcher,
+    query,
+    send
+  })
+);
