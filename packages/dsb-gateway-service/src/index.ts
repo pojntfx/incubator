b9363e1038b25300e4b7bbcba4c95fb70b845c39
Felix Pojtinger <felix@pojtinger.com>
2018-10-04 14:59:20 +0200

    feat(dsb-gateway-service): add simple scraper script

--- packages/dsb-gateway-service/src/index.ts
+++ packages/dsb-gateway-service/src/index.ts
@@ -1,1 +1,4 @@
-export default () => "Welcome to Micro";
+import { fetchDataFromDSB } from "./handlers/fetchDataFromDSB";
+
+export default async () =>
+  fetchDataFromDSB().then(() => "New Data has been fetched.");
