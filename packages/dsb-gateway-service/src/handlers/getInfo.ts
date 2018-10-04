1fdeb235db0712cd3089ad148dc3b5c64d0feb87
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-10-04 19:15:43 +0200

    feat(dsd-gateway-service): enable CORS

--- packages/dsb-gateway-service/src/handlers/getInfo.ts
+++ packages/dsb-gateway-service/src/handlers/getInfo.ts
@@ -1,6 +1,8 @@
-const getInfo = async (_, res, { db, send }) =>
-  await send(res, 200, {
-    lastFetchFromDSBDate: db.get("lastFetchFromDSBDate")
-  });
+const getInfo = async (_, res, { db, send, enableCORS }) =>
+  enableCORS(res).then(() =>
+    send(res, 200, {
+      lastFetchFromDSBDate: db.get("lastFetchFromDSBDate")
+    })
+  );
 
 export { getInfo };
