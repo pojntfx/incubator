86516dc593f45f1a272be0dde3be6c14a47efb1d
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-10-04 17:04:52 +0200

    feat(dsb-gateway-server): add lastUpdate date to list endpoint

--- packages/dsb-gateway-service/src/actions/getImageNames.ts
+++ packages/dsb-gateway-service/src/actions/getImageNames.ts
@@ -1,19 +1,13 @@
-import * as fs from "fs";
-
-const getImageNames = async (imagesPath, res, send) => {
-  await fs.readdir(imagesPath, (_, files) => {
-    try {
-      send(
-        res,
-        200,
-        files.map(file => ({
-          path: file
-        }))
-      );
-    } catch (e) {
-      send(res, 400, "Could not get list of images");
-    }
-  });
-};
+const getImageNames = async (imagesPath, dirReader, fileStatisticsGetter) =>
+  await dirReader(imagesPath).then(fileNames =>
+    Promise.all(
+      fileNames.map(async imageName => ({
+        path: imageName,
+        lastUpdate: await fileStatisticsGetter(imagesPath + imageName).then(
+          stats => stats.mtime
+        )
+      }))
+    )
+  );
 
 export { getImageNames };
