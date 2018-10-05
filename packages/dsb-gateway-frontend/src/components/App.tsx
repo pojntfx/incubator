d5d3d0bf5c3d7cee353b35d4705f8ac19feafbda
Felix Pojtinger <felix@pojtinger.com>
2018-10-05 21:28:19 +0200

    feat(dsb-gateway-frontend): get credentials from settings

--- packages/dsb-gateway-frontend/src/components/App.tsx
+++ packages/dsb-gateway-frontend/src/components/App.tsx
@@ -17,10 +17,27 @@ interface IApp {
   db: low;
 }
 
+const getEndpoint = (
+  listEndpoint,
+  dsbEndpoint,
+  staticEndpoint,
+  username,
+  password
+) =>
+  `${listEndpoint}?endpoint=${dsbEndpoint}&staticendpoint=${staticEndpoint}&username=${username}&password=${password}`;
+
 class App extends Component<IApp> {
   state = {
     settingsAreOpen: true,
-    endpoint: ""
+    endpoint: "",
+    data: {
+      settingsAreOpen: "",
+      username: "",
+      password: "",
+      listEndpoint: "",
+      staticEndpoint: "",
+      dsbEndpoint: ""
+    }
   };
 
   componentDidMount() {
@@ -34,7 +51,23 @@ class App extends Component<IApp> {
     } = this.props.db.read().__wrapped__;
     this.setState({
       settingsAreOpen,
-      endpoint: `${listEndpoint}?endpoint=${dsbEndpoint}&staticendpoint=${staticEndpoint}&username=${username}&password=${password}`
+      endpoint:
+        this.props.db.read().__wrapped__.endpoint ||
+        getEndpoint(
+          listEndpoint,
+          dsbEndpoint,
+          staticEndpoint,
+          username,
+          password
+        ),
+      data: {
+        settingsAreOpen,
+        username,
+        password,
+        listEndpoint,
+        staticEndpoint,
+        dsbEndpoint
+      }
     });
   }
 
@@ -45,34 +78,60 @@ class App extends Component<IApp> {
     });
   };
 
+  handleSettingsSave = data => {
+    this.setState({
+      data
+    });
+    this.props.db.set("endpoint", data.endpoint).write();
+    this.props.db.set("listEndpoint", data.listEndpoint).write();
+    this.props.db.set("dsbEndpoint", data.dsbEndpoint).write();
+    this.props.db.set("staticEndpoint", data.staticEndpoint).write();
+    this.props.db.set("username", data.username).write();
+    this.props.db.set("password", data.password).write();
+  };
+
   render() {
+    const endpoint = getEndpoint(
+      this.state.data.listEndpoint,
+      this.state.data.dsbEndpoint,
+      this.state.data.staticEndpoint,
+      this.state.data.username,
+      this.state.data.password
+    );
+
     return (
       <Container>
         <Navbar
+          data={this.state.data}
           settingsIsOpen={this.state.settingsAreOpen}
           onToggleSettings={this.toggleSettings}
+          onSettingsSave={this.handleSettingsSave}
         />
-        <Fetch url={this.state.endpoint}>
-          {({ loading, error, data }) => (
-            <div>
-              {loading && (
-                <Alert variant="info">
-                  <b>Loading ...</b> <br />
-                  DSB Gateway API Endpoint:{" "}
-                  <a href={this.state.endpoint}>{this.state.endpoint}</a>
-                </Alert>
-              )}
-              {error && (
-                <Alert variant="danger">
-                  <b>Oh no, an error occured!</b> Report this to{" "}
-                  <a href="https://www.twitter.com/@pojntfx">Felix Pojtinger</a>{" "}
-                  :<br /> {error.toString()}
-                </Alert>
-              )}
-              {data && <ImageList images={transformToImageList(data)} />}
-            </div>
-          )}
-        </Fetch>
+        {this.state.settingsAreOpen ? null : (
+          <Fetch url={endpoint}>
+            {({ loading, error, data }) => (
+              <div>
+                {loading && (
+                  <Alert variant="info">
+                    <b>Loading ...</b> <br />
+                    DSB Gateway API Endpoint:{" "}
+                    <a href={this.state.endpoint}>{this.state.endpoint}</a>
+                  </Alert>
+                )}
+                {error && (
+                  <Alert variant="danger">
+                    <b>Oh no, an error occured!</b> Report this to{" "}
+                    <a href="https://www.twitter.com/@pojntfx">
+                      Felix Pojtinger
+                    </a>{" "}
+                    :<br /> {error.toString()}
+                  </Alert>
+                )}
+                {data && <ImageList images={transformToImageList(data)} />}
+              </div>
+            )}
+          </Fetch>
+        )}
       </Container>
     );
   }
