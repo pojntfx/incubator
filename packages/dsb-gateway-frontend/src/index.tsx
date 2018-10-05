d283dea64b0fc92ff47e751a9ce04f8a76b880a5
Felix Pojtinger <felix@pojtinger.com>
2018-10-05 17:31:11 +0200

    feat(dsb-gateway-service, dsb-gateway-frontend): get static endpoint from query parameters

--- packages/dsb-gateway-frontend/src/index.tsx
+++ packages/dsb-gateway-frontend/src/index.tsx
@@ -4,7 +4,7 @@ import "./scss/main.scss";
 import { App } from "./components/App";
 
 render(
-  <App endpoint="http://localhost:3000/list?endpoint=https://www.dsbmobile.de/Login.aspx&username=296501&password=vplan2018" />,
+  <App endpoint="http://localhost:3000/list?endpoint=https://www.dsbmobile.de/Login.aspx&staticendpoint=http://localhost:4000&username=296501&password=vplan2018" />,
   document.getElementById("root")
 );
 
