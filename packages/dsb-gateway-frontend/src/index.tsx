5363f99457809a2cdf9576e315656f8b4c702456
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-10-05 19:04:16 +0200

    feat(dsb-gateway-frontend): start centralizing state

--- packages/dsb-gateway-frontend/src/index.tsx
+++ packages/dsb-gateway-frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { default as low } from "lowdb";
-import { default as LocalStorage } from "lowdb/adapters/LocalStorage";
+import low from "lowdb";
+import LocalStorage from "lowdb/adapters/LocalStorage";
 import "./scss/main.scss";
 import { App } from "./components/App";
 
@@ -17,9 +17,6 @@ db.defaults({
   dsbEndpoint: "https://www.dsbmobile.de/Login.aspx"
 }).write();
 
-render(
-  <App endpoint="http://localhost:3000/list?endpoint=https://www.dsbmobile.de/Login.aspx&staticendpoint=http://localhost:4000&username=296501&password=vplan2018" />,
-  document.getElementById("root")
-);
+render(<App db={db} />, document.getElementById("root"));
 
 export { App };
